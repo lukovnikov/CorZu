4cc91a0f14f829f6efe03b2e56f37228c932cd24
lukovnikov <lukovnikov@outlook.com>
2019-03-05 19:30:27 +0100

    qdsf

--- corzu.sh
+++ corzu.sh
@@ -16,8 +16,8 @@ echo "\nDone.\nExtracting markables..."
 python $corzu_dir/extract_mables_from_parzu.py $1".parzu" > $1".mables" #markable extraction
 echo "Done.\nResolving coreference..."
 python $corzu_dir/corzu.py $1".mables" $1".parzu" $1".coref"  #coreference resolution
-echo "\nDone.\nWriting HTML..."
-python $corzu_dir/conll_to_html.py $1".coref" > $1".html"
-echo "\nDone."
+#echo "\nDone.\nWriting HTML..."
+#python $corzu_dir/conll_to_html.py $1".coref" > $1".html"
+#echo "\nDone."
 #rm $1".mables" #uncomment to delete markable file
 #rm $1".parzu" #uncomment to delete parse file
